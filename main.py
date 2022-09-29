d62440c6e53121df4e2b492e2b6782ce49e107ff
eduardbopp <enichts9@gmail.com>
2022-09-29 03:22:22 +0200

    Fix main and offer better options

--- main.py
+++ main.py
@@ -8,10 +8,39 @@ from train.train import train
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+# First get our data
 traindt, testdt = get_data_mnist()
 
-loss_func = nn.CrossEntropyLoss()
+# Create a model
 model = CNN()
-model.eval()
-optimizer_LSR1 = LSR1(model.parameters())
-train(100, model, 256, optimizer_LSR1, traindt, testdt, loss_func)
+
+# Define loss function
+loss_func = nn.CrossEntropyLoss()
+
+# Set Epochs
+epochs = 100
+
+# Set batch size
+batch_size = 256
+
+# Set hyperparametrs
+lr = None
+max_iter = 20
+tolerance_grad = 1e-15
+tolerance_change = 1e-15
+tr_radius = 0.00075
+history_size = 3
+mu = 0.75
+nu = 0.75
+alpha_S = 0
+newton_maxit = None
+cg_iter = None
+line_search_fn = "strong_wolfe"
+trust_solver = "Steihaug_cg"
+
+#Define the optimizer
+optimizer_LSR1 = LSR1(model.parameters(), lr, max_iter, tolerance_grad, tolerance_change, tr_radius, history_size, mu,
+                      nu, alpha_S, newton_maxit, cg_iter, line_search_fn, trust_solver)
+
+#Train
+train(epochs, model, batch_size, optimizer_LSR1, traindt, testdt, loss_func)
